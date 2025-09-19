643a034ac157710f3aee5f04f3d41fdac6b5989b
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-09-19 17:14:21 +0545

    Order model build finished

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -21,10 +22,20 @@ class Product(models.Model):
         return self.name
 
 class Order(models.Model):
-    order_id = models.UUIDField(primary_key=True)
+    class StatusChoices(models.TextChoices):
+        PENDING = 'pending'
+        CONFIRMED = "confirmed"
+        CANCELLED = 'cancelled'
+
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
-    # status = models.CharField(choices=)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10,
+        choices=StatusChoices.choices,
+        default= StatusChoices.PENDING)
+    
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
