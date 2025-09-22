868c84e823298fa95a6adea425d2cf994ed05093
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-09-22 14:18:57 +0545

    Order list created according to the authenticated user

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 
 # def product_list(request):
 #     products = Product.objects.all()
@@ -71,6 +72,7 @@ class OrderListAPIView(generics.ListAPIView):
 class UserOrderListAPIView(generics.ListAPIView):
     queryset = Order.objects.prefetch_related('items__product')
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         qs = super().get_queryset()
