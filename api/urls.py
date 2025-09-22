246b2c42f7361dc803e2b12846399698dc177909
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-09-22 13:26:16 +0545

    created dynamic order filtering for the authenticated user

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('products/', views.ProductListAPIView.as_view()),
     path('product/<int:pk>/', views.ProductDetailAPIView.as_view()),
     path('product/info/', views.product_info),
-    path('orders/', views.OrderListAPIView.as_view())
+    path('orders/', views.OrderListAPIView.as_view()),
+    path('user-orders/', views.UserOrderListAPIView.as_view()),
 ]
