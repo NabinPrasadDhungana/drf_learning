20a4c0725f0aeb51d25f999ba9447aa85f4a8147
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-09-22 11:36:55 +0545

    urls using FBV removed

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('products/', views.product_list),
     path('products/', views.ProductListAPIView.as_view()),
-    # path('product/<int:pk>/', views.product_detail),
     path('product/<int:pk>/', views.ProductDetailAPIView.as_view()),
     path('product/info/', views.product_info),
     path('orders/', views.order_list)
