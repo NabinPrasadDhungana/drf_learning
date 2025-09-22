1dcf9b86c1dc9cb5820b57857e5e0cce371c7420
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-09-22 13:04:46 +0545

    admin.py modified for Inline forms of the related model

--- api/admin.py
+++ api/admin.py
@@ -3,5 +3,12 @@ from .models import Product, Order, OrderItem
 
 # Register your models here.
 admin.site.register(Product)
-admin.site.register(Order)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderItem)
