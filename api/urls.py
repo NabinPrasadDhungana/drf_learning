c7a05819f248f299b16ad847d870ff4ab1057919
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-09-25 22:41:06 +0545

    ProductDeleteView created

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('products/', views.ProductListCreateAPIView.as_view()),
     path('product/<int:pk>/', views.ProductDetailAPIView.as_view()),
     path('product/info/', views.ProductInfoAPIView.as_view()),
+    path('product/update/<int:pk>/', views.ProductUpdateAPIView.as_view()),
     path('orders/', views.OrderListAPIView.as_view()),
     path('user-orders/', views.UserOrderListAPIView.as_view()),
 ]
