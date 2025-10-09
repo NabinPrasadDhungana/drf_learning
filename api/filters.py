3b01e508eafa32952846daa19c9b40d4c1ce066e
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-10-09 12:40:53 +0545

    OrderFilter filterset class created to filter order

--- api/filters.py
+++ api/filters.py
@@ -1,7 +1,15 @@
 import django_filters
-from .models import Product
+from .models import Product, Order
 from rest_framework import filters
 
 class InStockFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         return queryset.filter(stock__gt=0)
+    
+class OrderFilter(django_filters.FilterSet):
+    class Meta:
+        model = Order
+        fields = {
+            'status': ['exact'],
+            'created_at': ['lt', 'gt', 'exact'],
+        }
