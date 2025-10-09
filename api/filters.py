ad9a23ec21f89c084b8e73e8b9987773686abe0b
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-10-09 14:27:50 +0545

    created_at field in the orderfilter filterset class created to filter by date only rather than both the date and time

--- api/filters.py
+++ api/filters.py
@@ -7,6 +7,7 @@ class InStockFilterBackend(filters.BaseFilterBackend):
         return queryset.filter(stock__gt=0)
     
 class OrderFilter(django_filters.FilterSet):
+    created_at = django_filters.DateFilter(field_name='created_at__date')
     class Meta:
         model = Order
         fields = {
